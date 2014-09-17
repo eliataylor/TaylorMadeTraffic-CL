106f53be0e7cf7d206f045acaf483650552ed9e8
taylormade415 <eliabrahamtaylor@gmail.com>
2014-09-17 01:22:25 -0700

    production ready

--- wwwroot/js/cubemanager.js
+++ wwwroot/js/cubemanager.js
@@ -401,4 +401,29 @@ $(document).ready(function() {
         tmt.initPreloader();
     }
     tmt.initPage(document.body);
+    /*
+    var xAngle = 0, yAngle = 0;
+    document.addEventListener('keydown', function(e) {
+      switch(e.keyCode) {
+
+        case 37: // left
+          yAngle -= 90;
+          break;
+
+        case 38: // up
+          xAngle += 90;
+          break;
+
+        case 39: // right
+          yAngle += 90;
+          break;
+
+        case 40: // down
+          xAngle -= 90;
+          break;
+      };
+
+      document.getElementById('domCube').style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
+    }, false);
+    */
 });
