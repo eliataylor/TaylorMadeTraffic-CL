a9cfd80a0dddb6e6b734e27071ab0e0510149c40
eliataylor <eli@taylormadetraffic.com>
2023-12-24 23:31:28 -1000

    renaming to capitals

--- web/application/models/Lenguaplus_model.php
+++ web/application/models/Lenguaplus_model.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class LenguaPlus_model extends CI_Model {
+class Lenguaplus_model extends CI_Model {
 
         private $count = false;
 
