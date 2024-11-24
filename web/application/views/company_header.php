1935cc13bd012db3eab08c3707cbd4039f12db3e
Eli Taylor <eli@taylormadetraffic.com>
2024-11-24 10:12:26 -0800

    using material icons

--- web/application/views/company_header.php
+++ web/application/views/company_header.php
@@ -54,8 +54,8 @@ $variant = $index % 2 == 0 ? 'footer' : 'header';
             </div>
 
             <span  class="caret closed" >
-                <svg width="10" height="30" viewBox="0 0 10 30" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 15 L0 0 L0 2 L8 15 L0 28 L0 30 L10 15 Z" />
+                <svg width="24" height="24" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
                 </svg>
             </span>
         </div>
