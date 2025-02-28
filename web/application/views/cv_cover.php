8424e39aeef766e681c878f87f051c6b7f973042
Eli Taylor <eli@taylormadetraffic.com>
2025-02-28 01:56:04 -0800

    cover

--- web/application/views/cv_cover.php
+++ web/application/views/cv_cover.php
@@ -9,24 +9,24 @@
     </div>
 
     <section style="font-size: 100%; line-height: 22px; margin:0 auto 0 auto; text-align: justify">
-        <p style="font-weight: 800">
-            Over the past 20 years, I've mastered the frontend and backend of several stacks, while both leading and assisting teams
-            through all life cycles of software development, from planning to deployment.
+        <p>
+            Over the past 20 years, I've mastered the frontend and backend of several stacks while leading and assisting through all life cycles of software development, from planning to deployment.
         </p>
         <p>
-            This resume highlights the projects most relevant to your job description and is listed by date rather than company.
-            By the nature of freelance contracting, my work with clients has spanned many years with gaps in between sprints.
+            This resume begins in 2017 when I met a talented graphic designer and launched a design and development agency. We spent the next eight years building teams, products, and applications while traveling the world and starting a family.
         </p>
         <p>
-            Cypher and Flexible Assembly Systems have been my most consistent clients over the past 7 years. Their
-            recommendations on my LinkedIn attest to my work ethic and customer satisfaction. As for code quality, I
-            approach every project with the best practices of test-driven development, while pen-testing my servers and
-            interfaces for security and performance under high traffic.</p>
-        <p>
-            In parallel, I invested time in myself through open source and proprietary ventures that have significantly enriched my professional skill set. This work has not only fueled my personal growth but also directly contributed to the innovative solutions I’ve implemented for my clients.
+            For evidence of my leadership, please review our full portfolio - <a href="https://taylormadetraffic.com/years" target="_blank">taylormadetraffic.com/years</a> - which includes many other projects and the teams I led to successful launches.
+            However, this resume lists specifically my own technical achievements and contributions.
         </p>
         <p>
-            For screenshots, videos, links and team members on each project visit my <strong>online portfolio: <a href="https://taylormadetraffic.com" target="_blank">taylormadetraffic.com</a></strong>
+            As you'll note from the overlapping dates, FieldWorks, Cypher LLC, and Flexible Assembly Systems have been my most consistent clients over these years.
+            Any concern about conflicts of interest can be alleviated by reading their respective recommendations on my LinkedIn or speaking with superiors who can attest to my work ethic, execution, and technical skill.
+        </p>
+        <p>
+            My work was valued so highly that I eventually took on a full-time role with Cypher for several years as Director of three teams of three to five engineers each, in addition to steering ongoing RFPs and overall business development.
+        <p>
+            Now that our son is four, we've decided to settle back home in the Bay Area to be near family and give him a more stable environment and community. In turn, it seems like an ideal time to consolidate my efforts and take on a more focused opportunity like yours.
         </p>
     </section>
 
